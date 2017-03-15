8dc84b1e13e9967cc6aac2eb0349f5a45312b9e6
syamamoto-pastoraldog <shimpei@pd-sy.local>
2017-03-15 22:04:42 +0900

    add test for ShowSpa

--- src/api/route.go
+++ src/api/route.go
@@ -2,16 +2,16 @@ package api
 
 import (
 	"fmt"
-	"net/http"
 	"github.com/gorilla/mux"
+	"net/http"
 )
 
 func Run() {
 	r := mux.NewRouter()
-	r.HandleFunc("/v1/ci",        Ci         ).Methods("GET")
-	r.HandleFunc("/v1/spas",      ShowSpaList).Methods("GET")
-	r.HandleFunc("/v1/spas/{id}", ShowSpa    ).Methods("GET")
-	r.HandleFunc("/v1/spas",      CreateSpa  ).Methods("POST")
+	r.HandleFunc("/v1/ci", Ci).Methods("GET")
+	r.HandleFunc("/v1/spas", ShowSpaList)
+	r.HandleFunc("/v1/spa/{id}", ShowSpa)
+	r.HandleFunc("/v1/spa", CreateSpa).Methods("POST")
 	http.Handle("/", r)
 
 	fmt.Printf("Server is running... localhost:8080")
--- src/test/spa_test.go
+++ src/test/spa_test.go
@@ -1,35 +1,75 @@
 package test
 
 import (
-  "api"
+	"api"
+	"encoding/json"
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	_ "net/url"
+	_ "strings"
 	"testing"
-	"encoding/json"
 )
 
+func TestShowSpa(t *testing.T) {
+	req, _ := http.NewRequest("GET", "v1/spa/1", nil)
+	res := httptest.NewRecorder()
+	api.ShowSpa(res, req)
+
+	if res.Code != 200 {
+		t.Fatalf("not 200, %s", res.Code)
+	}
+
+	// data, _ := ioutil.ReadAll(res.Body)
+	// spa := new(api.Spa)
+	// json.Unmarshal(([]byte)(string(data)), spa)
+
+	// if spa.Name != "木下温泉" {
+	// 	t.Fatalf("not 木下温泉, %s", spa.Name)
+	// }
+	// if spa.Address != "北海道" {
+	// 	t.Fatalf("not 北海道, %s", spa.Address)
+	// }
+}
+
+func TestShowAnotherSpa(t *testing.T) {
+	// db.Query("INSERT INTO spa (id, name, address) VALUES(?, ?, ?)", 2, "木下温泉", "北海道")
+	// values := url.Values{}
+	// values.Set("id", "2")
+	// req, _ := http.NewRequest("GET", "v1/spa/2", strings.NewReader(values.Encode()))
+	// res := httptest.NewRecorder()
+	// api.ShowSpa(res, req)
+
+	// data, _ := ioutil.ReadAll(res.Body)
+	// spa := new(api.Spa)
+	// json.Unmarshal(([]byte)(string(data)), spa)
+
+	// if spa.Name != "木下温泉2" {
+	// 	t.Fatalf("not 木下温泉2, %s", spa.Name)
+	// }
+}
+
 func execShowSpaList() *api.Spas {
-  //ShowSpaList実行
+	//ShowSpaList実行
 	req, _ := http.NewRequest("GET", "v1/spas", nil)
 	res := httptest.NewRecorder()
 	api.ShowSpaList(res, req)
 
-  //レスポンスを構造体に変換
+	//レスポンスを構造体に変換
 	data, _ := ioutil.ReadAll(res.Body)
 	spas := new(api.Spas)
 	json.Unmarshal(([]byte)(string(data)), spas)
-  return spas
+	return spas
 }
 
 func TestShowSpaList(t *testing.T) {
-  db.Query("INSERT INTO spa (name, address) VALUES(?, ?)", "木下温泉", "北海道")
-  db.Query("INSERT INTO spa (name, address) VALUES(?, ?)", "木下温泉2", "北海道2")
+	db.Query("INSERT INTO spa (name, address) VALUES(?, ?)", "木下温泉", "北海道")
+	db.Query("INSERT INTO spa (name, address) VALUES(?, ?)", "木下温泉2", "北海道2")
 
-  result := execShowSpaList()
+	result := execShowSpaList()
 
-  assertEqual(t, result.Spas[0].Name,    "木下温泉")
-  assertEqual(t, result.Spas[0].Address, "北海道")
-  assertEqual(t, result.Spas[1].Name,    "木下温泉2")
-  assertEqual(t, result.Spas[1].Address, "北海道2")
+	assertEqual(t, result.Spas[0].Name, "木下温泉")
+	assertEqual(t, result.Spas[0].Address, "北海道")
+	assertEqual(t, result.Spas[1].Name, "木下温泉2")
+	assertEqual(t, result.Spas[1].Address, "北海道2")
 }
