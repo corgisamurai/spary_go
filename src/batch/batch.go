86f613d3c463f6489381bd28ba30a8f7e7d8507d
hitosaka21 <osaka708@gmail.com>
2017-05-17 21:01:17 +0900

    じゃらんの検索結果XMLをパースする

--- src/batch/batch.go
+++ src/batch/batch.go
@@ -1,11 +1,41 @@
 package batch
 
 import (
+  "encoding/xml"
   "fmt"
   "io/ioutil"
 	_ "github.com/go-sql-driver/mysql"
 	"net/http"
 )
+
+type Results struct {
+  NumberOfResults string `xml:"NumberOfResults"`
+  DisplayPerPage string `xml:"DisplayPerPage"`
+  DisplayFrom string `xml:"DisplayFrom"`
+  APIVersion string `xml:"APIVersion"`
+  Onsen []Onsen `xml:"Onsen"`
+}
+
+type Onsen struct {
+  OnsenName string `xml:"OnsenName"`
+  OnsenNameKana string `xml:"OnsenNameKana"`
+  OnsenID string `xml:"OnsenID"`
+  OnsenAddress string `xml:"OnsenAddress"`
+  Area Area `xml:"Area"`
+  NatureOfOnsen string `xml:"NatureOfOnsen"`
+  OnsenAreaName string `xml:"OnsenAreaName"`
+  OnsenAreaNameKana string `xml:"OnsenAreaNameKana"`
+  OnsenAreaID string `xml:"OnsenAreaID"`
+  OnsenAreaURL string `xml:"OnsenAreaURL"`
+  OnsenAreaCaption string `xml:"OnsenAreaCaption"`
+}
+
+type Area struct {
+  Region string `xml:"Region"`
+  Prefecture string `xml:"Prefecture"`
+  LargeArea string `xml:"LargeArea"`
+  SmallArea string `xml:"SmallArea"`
+}
 
 func ImportOnsenList() {
   url := "http://jws.jalan.net/APICommon/OnsenSearch/V1/?key=aqr15a41839ced&l_area=010300&count=1&xml_ptn=1"
@@ -15,5 +45,13 @@ func ImportOnsenList() {
   defer resp.Body.Close()
 
   byteArray, _ := ioutil.ReadAll(resp.Body)
-  fmt.Println(string(byteArray))
+
+  results := Results{}
+  err := xml.Unmarshal(byteArray, &results)
+  if err != nil {
+    fmt.Println(string(byteArray))
+    return
+  }
+
+  fmt.Println(results)
 }
